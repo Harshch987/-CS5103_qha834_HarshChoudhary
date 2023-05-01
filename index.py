9dadff580e8c3aeb3f29fb0cdafcbb9b16f22798
Harshch987 <54935713+Harshch987@users.noreply.github.com>
2023-04-30 19:03:00 -0500

    Update index.py

--- index.py
+++ index.py
@@ -1,8 +1,28 @@
 """
-First requirement change
+Second requirement change
 """
 import re
+def change_word(text, pattern, replacement):
+    """
+    It replaces the words.
+    """
+    # Split the text into a list of words
+    words = re.split(r" |\t", text)
 
+    # Loop through each word and replace if it matches the pattern
+    for i in range(len(words)):
+        if words[i] == pattern and (i == 0 or not words[i-1].endswith("\t")):
+            words[i] = replacement
+
+    # Join the words back into a single string
+    changed_text = ' '.join(words)
+
+    return changed_text
+original_text = "ab cd ef"
+pattern = "ab"
+replacement = "cd"
+changed_text = change_word(original_text, pattern, replacement)
+print(f"Final Result: {changed_text}")
 #Replacing words
 def replace_word(input_text):
     """
@@ -20,10 +40,22 @@ with open("inp.txt", "r", encoding="utf-8") as file:
 # Count the number of lines in the text
 line_count = len(text.split('\n'))
 print(f"Number of lines: {line_count}")
-
 # Count the number of characters in the text
 char_count = len(text)
 print(f"Number of characters: {char_count}")
+
+def changed_word(text, pattern, replacement):
+    if pattern in text:
+        words = text.split()
+        new_words = []
+        for word in words:
+            if word == pattern:
+                new_words.append(replacement)
+            else:
+                new_words.append(word)
+        return " ".join(new_words)
+    else:
+        return "Pattern not found in text."
 
 
 def count_li(text):
